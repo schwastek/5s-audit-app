d086b20087fcdd249a556a947753331bd52ef8b2
schwastek <31923722+schwastek@users.noreply.github.com>
2022-02-19 13:57:56 +0100

    Initialize collection
    It prevents null reference exceptions. It is better to expose not-null collections since you avoid additional checks in your code (e.g. when reading from DB).

--- Api/Domain/Audit.cs
+++ Api/Domain/Audit.cs
@@ -18,7 +18,7 @@ namespace Api.Domain
         [Required]
         public DateTime EndDate { get; set; }
 
-        public ICollection<Answer> Answers { get; set; }
+        public ICollection<Answer> Answers { get; set; } = new List<Answer>();
 
         public double CalculateScore()
         {
