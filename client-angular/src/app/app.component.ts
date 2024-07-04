bf76e350cc2aee1c7e306f8a6206f8f743a9a960
Sebastian Chwastek <s.chwastek@outlook.com>
2024-07-04 20:31:27 +0200

    Clean up SCSS

--- client-angular/src/app/app.component.ts
+++ client-angular/src/app/app.component.ts
@@ -8,7 +8,6 @@ import { AccountService } from './account/account.service';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   title = '5S Audit App';
