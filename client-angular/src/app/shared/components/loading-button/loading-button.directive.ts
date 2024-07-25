0e86045e5157e6ad9f7653b70d4a28930b462a00
Sebastian Chwastek <s.chwastek@outlook.com>
2024-07-25 20:58:00 +0200

    Add Loading Button Directive

---
+++ client-angular/src/app/shared/components/loading-button/loading-button.directive.ts
@@ -0,0 +1,57 @@
+import { DOCUMENT } from '@angular/common';
+import { Directive, effect, ElementRef, inject, input } from '@angular/core';
+
+@Directive({
+  selector: '[appLoadingButton]',
+  standalone: true
+})
+export class LoadingButtonDirective {
+  loading = input<boolean>(false);
+  host = inject(ElementRef);
+  document: Document = inject(DOCUMENT);
+
+  private get htmlElement(): HTMLElement {
+    return this.host.nativeElement as HTMLElement;
+  }
+
+  constructor() {
+    effect(() => {
+      if (this.loading()) {
+        this.showIcon();
+      } else {
+        this.hideIcon();
+      }
+    });
+  }
+
+  showIcon() {
+    const iconElement = this.document.querySelector('.spinner-border');
+
+    if (iconElement) {
+      iconElement.classList.remove('d-none');
+    } else {
+      this.createIcon();
+    }
+  }
+
+  createIcon() {
+    const created = this.document.querySelector('.spinner-border');
+
+    if (!created) {
+      const iconElement = this.document.createElement('span');
+      iconElement.className = 'spinner-border spinner-border-sm me-1';
+      iconElement.setAttribute('aria-hidden', 'true');
+
+      // Insert before text.
+      this.htmlElement.insertBefore(iconElement, this.htmlElement.firstChild);
+    }
+  }
+
+  hideIcon() {
+    const iconElement = this.document.querySelector('.spinner-border');
+
+    if (iconElement) {
+      iconElement.classList.add('d-none');
+    }
+  }
+}
