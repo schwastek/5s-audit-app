27c4b8d888486ffdef5756e538dc9a861cd356e3
Sebastian Chwastek <s.chwastek@outlook.com>
2024-08-15 22:24:14 +0200

    Get audits using subscribe

--- client-angular/src/app/audit/audit-list/audit-list.component.ts
+++ client-angular/src/app/audit/audit-list/audit-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { AuditService } from '../audit.service';
 import { PaginatedResult } from '../../shared/models/pagination';
 import { RouterLink } from '@angular/router';
-import { BehaviorSubject, catchError, finalize, Observable, switchMap, tap } from 'rxjs';
 import { PaginationComponent } from '../../shared/components/pagination/pagination.component';
 import { AuditListItemDto } from '../models/audit.models';
 import { AsyncPipe } from '@angular/common';
@@ -18,43 +17,36 @@ import { AsyncPipe } from '@angular/common';
   ]
 })
 export class AuditListComponent implements OnInit {
-  public audits$!: Observable<PaginatedResult<AuditListItemDto>>;
-
-  private currentPage = new BehaviorSubject<number>(1);
-  public currentPage$ = this.currentPage.asObservable();
-
+  public audits = new PaginatedResult<AuditListItemDto>();
   public error: string | null = null;
-  public isLoading = true;
+  public isLoading = false;
 
-  constructor(
-    private auditService: AuditService
-  ) {}
+  constructor(private auditService: AuditService) {}
 
   ngOnInit() {
-    this.getAudits();
+    this.getAudits(1);
   }
 
-  private getAudits() {
-    // Get audits when page changes.
-    this.audits$ = this.currentPage$.pipe(
-      tap(() => {
-        this.isLoading = true;
-      }),
-      switchMap((pageNumber) => {
-        return this.auditService.getAudits(pageNumber).pipe(
-          catchError((error) => {
-            this.error = 'Failed to fetch audits.';
-            throw error;
-          }),
-          finalize(() => {
-            this.isLoading = false;
-          })
-        );
-      })
-    );
+  private getAudits(pageNumber: number) {
+    this.isLoading = true;
+
+    this.auditService.getAudits(pageNumber).subscribe({
+      next: (audits) => {
+        this.audits = audits;
+      },
+      error: (_) => {
+        this.error = 'Failed to fetch audits.';
+        this.isLoading = false;
+      },
+      complete: () => {
+        this.isLoading = false;
+      }
+    });
   }
 
   onPageChange(pageNumber: number) {
-    this.currentPage.next(pageNumber);
+    // Items are hidden during page changes until loading is complete.
+    this.audits.items = [];
+    this.getAudits(pageNumber);
   }
 }
