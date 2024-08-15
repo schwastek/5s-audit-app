49964263a43fc2fb33b0600cbaf36da7fa0e38c1
Sebastian Chwastek <s.chwastek@outlook.com>
2024-08-15 21:47:22 +0200

    Get audits using async pipe

--- client-angular/src/app/audit/audit-list/audit-list.component.ts
+++ client-angular/src/app/audit/audit-list/audit-list.component.ts
@@ -2,49 +2,59 @@ import { Component, OnInit } from '@angular/core';
 import { AuditService } from '../audit.service';
 import { PaginatedResult } from '../../shared/models/pagination';
 import { RouterLink } from '@angular/router';
-import { firstValueFrom } from 'rxjs';
-import { HttpErrorResponse } from '@angular/common/http';
+import { BehaviorSubject, catchError, finalize, Observable, switchMap, tap } from 'rxjs';
 import { PaginationComponent } from '../../shared/components/pagination/pagination.component';
 import { AuditListItemDto } from '../models/audit.models';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-audit-list',
   templateUrl: './audit-list.component.html',
   standalone: true,
   imports: [
+    AsyncPipe,
     RouterLink,
     PaginationComponent
   ]
 })
 export class AuditListComponent implements OnInit {
-  audits: PaginatedResult<AuditListItemDto> | null = null;
-  error: string | null = null;
-  isLoading: boolean | null = false;
+  public audits$!: Observable<PaginatedResult<AuditListItemDto>>;
+
+  private currentPage = new BehaviorSubject<number>(1);
+  public currentPage$ = this.currentPage.asObservable();
+
+  public error: string | null = null;
+  public isLoading = true;
 
   constructor(
     private auditService: AuditService
   ) {}
 
-  async ngOnInit() {
-    await this.getAudits(1);
+  ngOnInit() {
+    this.getAudits();
   }
 
-  async getAudits(pageNumber: number) {
-    try {
-      this.isLoading = true;
-      this.audits = await firstValueFrom(this.auditService.getAudits(pageNumber));
-    } catch (err: unknown) {
-      if (err instanceof HttpErrorResponse) {
-        this.audits = null;
-        this.error = err.message;
-      }
-      throw err;
-    } finally {
-      this.isLoading = false;
-    }
+  private getAudits() {
+    // Get audits when page changes.
+    this.audits$ = this.currentPage$.pipe(
+      tap(() => {
+        this.isLoading = true;
+      }),
+      switchMap((pageNumber) => {
+        return this.auditService.getAudits(pageNumber).pipe(
+          catchError((error) => {
+            this.error = 'Failed to fetch audits.';
+            throw error;
+          }),
+          finalize(() => {
+            this.isLoading = false;
+          })
+        );
+      })
+    );
   }
 
-  async onPageChange(pageNumber: number) {
-    await this.getAudits(pageNumber);
+  onPageChange(pageNumber: number) {
+    this.currentPage.next(pageNumber);
   }
 }
