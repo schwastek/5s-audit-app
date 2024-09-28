06aa6b5fce4f53689fe269da8135d4c00b7bcc4c
Sebastian Chwastek <s.chwastek@outlook.com>
2024-09-28 11:16:20 +0200

    Add custom AbstractValidator
    Validate null objects (e.g. when request is sent with an empty request body).

---
+++ api/Core/ValidatorService/CustomAbstractValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+
+namespace Core.ValidatorService;
+
+public class CustomAbstractValidator<T> : AbstractValidator<T>
+{
+    protected override bool PreValidate(ValidationContext<T> context, ValidationResult result)
+    {
+        // If we send a request with an empty request body.
+        if (context.InstanceToValidate is null)
+        {
+            result.Errors.Add(new ValidationFailure("", "The request is empty or invalid."));
+
+            return false;
+        }
+
+        return true;
+    }
+}
