f438eae6fcf338009e63160b9b7ce844b3fd3cc8
Sebastian Chwastek <s.chwastek@outlook.com>
2025-02-23 12:01:00 +0100

    Use FallbackPolicy to require authenticated users
    Require authentication by default with FallbackPolicy.

--- api/Api/Program.cs
+++ api/Api/Program.cs
@@ -9,7 +9,6 @@ using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -20,10 +19,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers(opt =>
 {
-    // Require authenticated users. Add a default AuthorizeFilter to all endpoints.
-    AuthorizationPolicy policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
-    opt.Filters.Add(new AuthorizeFilter(policy));
-
     // In enabled Nullable context, the validation system treats non-nullable parameters or bound properties
     // as if they had a [Required(AllowEmptyStrings = true)] attribute.
     // A missing value for Name in a JSON or form post results in a validation error. Disable it.
@@ -34,6 +29,24 @@ builder.Services.AddControllers(opt =>
     // Both model binding and model validation occur before the execution of a controller action.
     opt.SuppressModelStateInvalidFilter = true;
 });
+
+builder.Services.AddAuthorization(opt =>
+{
+    // Requires all users to be authenticated by default, providing a more secure approach
+    // compared to manually adding the [Authorize] attribute to new controllers.
+    // The fallback authorization policy applies to all requests that do not explicitly specify 
+    // an authorization policy, such as [Authorize(Policy = "MyPolicy")] or [AllowAnonymous].
+    // FallbackPolicy is used instead of DefaultPolicy because DefaultPolicy only applies
+    // when an endpoint has [Authorize] without a specified policy.
+    // In contrast, FallbackPolicy is enforced globally, even on endpoints without [Authorize].
+    // Setting FallbackPolicy is preferred over applying an authorization filter to all controllers..
+    // See: https://learn.microsoft.com/en-us/aspnet/core/security/authorization/secure-data?view=aspnetcore-8.0#require-authenticated-users
+    var policy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+    opt.FallbackPolicy = policy;
+});
+
 builder.Services.AddDbContext<LeanAuditorContext>();
 
 // Register the Swagger generator
